a4bc6ef6de8af3ff46593e09fa643894f8f19bbe
mustafa hegy <mustafa.hegy94@gmail.com>
2022-10-08 21:56:32 +0200

    Initial Commit

---
+++ InstgramApp/InstgramAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstgramAppApp.swift
+//  InstgramApp
+//
+//  Created by Mostafa on 08/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstgramAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
